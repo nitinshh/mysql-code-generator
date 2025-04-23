a6b05793a81615dc63c35195a96ff05f86908f26
nitinshh <nitinsharma30302@gmail.com>
2025-04-23 11:32:46 +0530

    use cluster this time

--- app.js
+++ app.js
@@ -1,72 +1,79 @@
-require("dotenv").config();
-const createError = require("http-errors");
+const cluster = require("cluster");
+const os = require("os");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const fileUpload = require("express-fileupload");
-const swaggerUi = require("swagger-ui-express");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/userRoute")();
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  console.log(`Number of CPUs: ${numCPUs}`);
+  console.log(`Master ${process.pid} is running`);
+  console.log(`Forking ${numCPUs} workers...`);
 
-const app = express();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-require("./dbConnection").connectdb();
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Forking a new one...`);
+    cluster.fork();
+  });
 
-// view engine setup
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+} else {
+  // Worker process
+  require("dotenv").config();
+  const createError = require("http-errors");
+  const path = require("path");
+  const cookieParser = require("cookie-parser");
+  const logger = require("morgan");
+  const fileUpload = require("express-fileupload");
+  const swaggerUi = require("swagger-ui-express");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+  const indexRouter = require("./routes/index");
+  const usersRouter = require("./routes/userRoute")();
+  const { connectdb } = require("./dbConnection");
 
-// Enable file upload using express-fileupload
-app.use(
-  fileUpload({
+  const app = express();
+  const PORT = process.env.PORT || 4000;
+
+  connectdb();
+
+  app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "views"));
+
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "public")));
+
+  app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-  })
-);
+  }));
 
-const swaggerOptions = {
-  explorer: true,
-  swaggerOptions: {
-    urls: [
-      {
-        url: "/user",
-        name: "User API",
-      },
-      {
-        url: "/business",
-        name: "Business API",
-      },
-    ],
-  },
-};
+  const swaggerOptions = {
+    explorer: true,
+    swaggerOptions: {
+      urls: [
+        { url: "/user", name: "User API" },
+        { url: "/business", name: "Business API" },
+      ],
+    },
+  };
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, swaggerOptions));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, swaggerOptions));
+  app.use("/", indexRouter);
+  app.use("/users", usersRouter);
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+  app.use((req, res, next) => next(createError(404)));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+  app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
+  });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+  app.listen(PORT, () => {
+    console.log(`✅ Worker ${process.pid} running on port ${PORT}`);
+  });
+}
--- dbConnection.js
+++ dbConnection.js
@@ -1,3 +1,5 @@
+const numCPUs = require("os").cpus().length;
+
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
@@ -6,6 +8,15 @@ const sequelize = new Sequelize(
   {
     host: process.env.DATABASE_HOST,
     dialect: process.env.DIALECT,
+    logging: false, // Optional: disable SQL logging in console
+
+    // Connection pool settings
+    pool: {
+      max: numCPUs,      // maximum number of connection in pool
+      min: 0,          // minimum number of connection in pool
+      acquire: 30000,  // max time (ms) that pool will try to get connection before throwing error
+      idle: 10000      // max time (ms) that a connection can be idle before being released
+    }
   }
 );
 
@@ -14,7 +25,7 @@ const connectdb = async () => {
     .authenticate()
     .then(async () => {
       await sequelize.sync({ alter: false });
-      console.log("db is connected and sync also");
+      // console.log("db is connected and sync also");
     })
     .catch((err) => {
       console.log("error while connecting to the db", err);
